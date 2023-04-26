25ff13edf081a68ebdcf6c63ae65a2c9ecf866ad
arnaudhuyssen <arnaud.huyssen@insee.fr>
2023-04-26 15:00:51 +0000

    Initialisation du projet

---
+++ dev.R
@@ -0,0 +1,5 @@
+# install.packages("remotes") si necessaire
+remotes::install_github("InseeFrLab/doremifasol", build_vignettes = TRUE)
+
+library(tidyverse)
+library(dplyr)
---
+++ doremifasol_donnees.R
@@ -0,0 +1,3 @@
+donnees_rp <- doremifasol::telechargerDonnees("RP_INDREG", date = 2016, vars = c("REGION", "IPONDI", "SEXE", "AGED"))
+
+donnees_filosofi <- doremifasol::telechargerDonnees("FILOSOFI_DISP_COM_ENS", date = 2017)
