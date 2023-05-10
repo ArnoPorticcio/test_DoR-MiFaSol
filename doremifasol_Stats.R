d5c327e12f17890fbe95997715c251e2ca3f4f72
arnaudhuyssen <arnaud.huyssen@insee.fr>
2023-05-10 12:57:07 +0000

    Commit Table RP

--- doremifasol_Stats.R
+++ doremifasol_Stats.R
@@ -1,8 +1,16 @@
-test<-table(donnees_rp_lgt$REGION,donnees_rp_lgt$CATL)
-test_lbl<-table(donnees_rp_lgt$REGION_Lbl,donnees_rp_lgt$CATL_Lab)
+#STATS
 
+# Type de logement : Source RP
+RP_Catl<-table(donnees_rp_lgt$REGION_Lbl,donnees_rp_lgt$CATL_Lab) # En volume
+RP_Catl_prop<-100*prop.table(RP_Catl,1)
+RP_Catl_prop_DF<-as.data.frame.matrix(RP_Catl_prop)
 
+# Type de logement : Source Filosofi
+FILO_Catl<-table(donnees_filo$REG
 
+                 
+head(donnees_filo)
+                 
 prop.table(test,1)
 
 
